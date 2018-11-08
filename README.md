c5994c4351bbc9fe6d4f6d66dbc43ccc96b684ae
thisiscodingnow <32372917+thisiscodingnow@users.noreply.github.com>
2018-11-08 00:55:07 -0500

    hw8 v.2

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Liri-Bot
+# liri-node-app
+
+LIRI is a Language Interpretation and Recognition Interface. 
+LIRI will be a command line node app that takes in parameters and gives you back data.
+
+github-page:https://bethgmariam.github.io/liri-node-app/
+
+Requirements:
+
+Get API keys:
+1. Spotify API keys(SPOTIFY_ID & SPOTIFY_SECRET)... use Node-Spotify-API
+2. Twitter API keys(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET,TWITTER_ACCESS_TOKEN_KEY,TWITTER_ACCESS_TOKEN_SECRET)
+3. OMDB API (Open Movies DataBase) 
+
+
+
+Install all required npm packages. 
+
+npm install dotenv,
+npm install require,
+npm install twitter,
+npm install spottify,
+
+
+How to use this App. 
+
+Step1.
+
+Use the following parameters for searchOptions (process.argv[2])
+
+* my-tweets
+
+* spotify-this-song
+
+* movie-this
+
+* do-what-it-says
+
+Step2
+
+* Type values for searchTitle(process.argv[3]). 
+
+
+
+
--- liri.js
+++ liri.js
@@ -1,158 +1,229 @@
+//At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
 require("dotenv").config();
-var keys = require("./keys");
+var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 var request = require("request");
-var Spotify = require('node-spotify-api');
-var Twitter = require('twitter');
 var fs = require("fs");
-
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var searchOption = process.argv[2];
 
-var action = process.argv[2];
-var arg1 = process.argv[3];
-var nodeArg = process.argv;
-var nodeArr = [];
-for (var i = 3; i < nodeArg.length; i++) {
-    nodeArr.push(nodeArg[i]);
-};
-var nodeValue = nodeArr.join("+");
 
-var queryUrl = "http://www.omdbapi.com/?t=" + nodeValue + "&y=&plot=short&apikey=trilogy";
-var options = {
-    url: queryUrl,
-    method: "GET",
-};
+// Take in the command line arguments 
+var nodeArgs = process.argv;
 
-function tweets() {
-    var params = { screen_name: "MrFullStack1" };
-    client.get("statuses/user_timeline", params, function (err, tweets, res) {
-        if (err) {
-            console.log("Error occurred: " + (err));
-        } else {
-            for (var i = 0; i < tweets.length; i++) {
-                console.log("Tweets: \n  Tweet #" + (i + 1) + "\n  Text: " + tweets[i].text + "\n  Created on: " + tweets[i].created_at);
-                console.log("*********************************");
-            };
-            fs.appendFile("log.txt", "\nChecked Tweets - " + new Date(), function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("log.txt was updated!");
-                };
-            });
-        };
-    });
-};
+// Create an empty string for holding the searchTitle
+var searchTitle = "";
 
-function spotifySong() {
-    spotify.search({
-        type: "track",
-        query: nodeValue
-    }, function (err, data) {
-        if (err) {
-            console.log("Error occurred: " + err);
-        } else {
-            console.log("*********************************");
-            for (i = 0; i < data.tracks.items.length; i++) {
-                console.log("Result # " + (i + 1));
-                console.log("Artist: " + data.tracks.items[i].artists[0].name);
-                console.log("Track: " + data.tracks.items[i].name);
-                console.log("Album: " + data.tracks.items[i].album.name);
-                console.log("Explicit content: " + data.tracks.items[i].explicit);
-                console.log("Link to song: " + data.tracks.items[i].external_urls.spotify);
-                console.log("*********************************");
-            };
-            fs.appendFile("log.txt", "\nSpotified " + nodeValue + " - " + new Date(), function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("log.txt was updated!");
-                };
-            });
-        };
-    });
-};
+// Capture all the words in the title(which starts from argument[3])
+for (var i=3 ; i < nodeArgs.length; i++) {
 
-function movie() {
-    request(options, function (err, res, body) {
-        if (err) {
-            console.log("Error occurred: " + err);
-        } else {
-            var json = JSON.parse(body);
-            console.log("*********************************");
-            console.log("Title: " + json.Title);
-            console.log("Director: " + json.Director);
-            console.log("Release date: " + json.Released);
-            console.log("Rating: " + json.Rated);
-            console.log("Language: " + json.Language);
-            console.log("Plot: " + json.Plot);
-            console.log("Cast: " + json.Actors);
-            console.log("Ratings: \n  IMDB: " + json.Ratings[0].Value + "\n  Rotten Tomatoes: " + json.Ratings[1].Value + "\n  Metacritic: " + json.Ratings[2].Value);
-            console.log("*********************************");
-            fs.appendFile("log.txt", "\nOMBDed " + nodeValue + " - " + new Date(), function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("log.txt was updated!");
-                };
-            });
-        };
-    });
-};
+// Build a string.
+searchTitle = searchTitle + " " + nodeArgs[i];
+// trim spaces (if any)
+searchTitle = searchTitle.trim();
+}
 
-function command() {
-    fs.readFile("random.txt", "utf8", function (err, data) {
-        if (err) {
-            console.log("Error occurred: " + err);
-        } else {
-            var dataArr = data.split(",");
-            for (var i = 1; i < dataArr.length; i++) {
-                nodeArr.push(dataArr[i]);
-            };
-            nodeValue = nodeArr.join("+");
-            if (dataArr[0] == "my-tweets") {
-                tweets();
-            } else if (dataArr[0] == "spotify-this-song") {
-                spotifySong();
-            } else if (dataArr[0] == "movie-this") {
-                movie();
-            } else {
-                console.log("Error occured");
-            };
-            fs.appendFile("log.txt", "\nUsed do-what-it-says - " + new Date(), function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("log.txt was updated!");
-                };
-            });
-        };
-    });
-};
+// switch-case statement  based on search options & call respective functions
+switch(searchOption){
 
-if (action == "my-tweets") {
-    tweets();
-} else if (action == "spotify-this-song") {
-    if (arg1 === undefined) {
-        nodeValue = "the+sign+ace+of+base";
-        spotifySong();
-    } else {
-        spotifySong();
+    case "my-tweets":
+    getTweets();
+    break;
+
+    case "spotify-this-song":
+    getSpotify();
+    break;
+
+    case "movie-this":
+    getMovie();
+    break;
+
+    case "do-what-it-says":
+    getDoWhatItSays();
+    break;
+
+    default:
+    // if user types node liri.js (without argument), display the following instruction
+    console.log("Select input option:\n" +
+    "eg. my-tweets,\n spotify-this-song,\n movie-this & \n do-what-it-says(this option gets value from random.txt file).\n"+
+    "After picking option,add value to the option (where needed) \neg. movie-this 'Black Panthers'. \n"+
+    "LIRI will display default ... if you only pick option & no value")
+    
+}
+
+    // twitter  
+    function getTweets(){
+        var tempArray=[]// this array is for later use by writeTolog  function;
+        //accordig to tweeter documentation, count specifies the number of Tweets.
+    
+        var params = {screen_name: 'Beth69058147',count:20};
+    
+        client.get('statuses/user_timeline',params, function(error, tweets, response) {
+            if (!error) {
+                // console.log(tweets);
+                for(var i=0; i<tweets.length;i++){
+                var tempTweets = tweets[i].text;
+                var tempDateCreated = tweets[i].created_at;
+                console.log("Tweet"+[tweets.length-i]+": "+tempTweets);
+                console.log( "Date Created: "+ tempDateCreated);
+                console.log("\n----------------------------------------------------\n")
+            
+                var getTweetslog= {
+                        searchCommand1:searchOption,
+                        Tweets:tempTweets,
+                        DateCreated: tempDateCreated
+                    }
+
+                        tempArray.push(getTweetslog);
+                }
+            // write output to log.txt after pushing each tweets to an array
+            writeTolog(tempArray)
+
+            }
+        });
     };
-} else if (action == "movie-this") {
-    if (arg1 === undefined) {
-        nodeValue = "mr+nobody";
-        queryUrl = "http://www.omdbapi.com/?t=" + nodeValue + "&y=&plot=short&apikey=trilogy";
-        options = {
-            url: queryUrl,
-            method: "GET",
-        };
-        movie();
-    } else {
-        movie();
+
+    //spotify 
+
+    function getSpotify(){
+
+        // console.log(spotify);
+    
+        var title = "";
+        // if user has input value use that as a title 
+        if(searchTitle){
+    
+            title = searchTitle;
+        }
+        // else use Mr.Nobody as default title.
+        else{
+            title = "The Sign";
+        }
+    
+        // var spotify = require('spotify');
+     
+        spotify.search({ type: 'track', query: title }, function(err, data) {
+
+
+            if ( err ) {
+                console.log('Error occurred: ' + err);
+                return;
+            }        
+            // console.log(data.tracks.items[0]);
+            var tempArtistName=data.tracks.items[0].album.artists[0].name;
+            var tempSongName= data.tracks.items[0].name;
+            var tempURL=data.tracks.items[0].album.external_urls.spotify;
+            var tempAlbumName=data.tracks.items[0].album.name ;
+
+            console.log("Artist Name: "+tempArtistName +"\n");
+            console.log("Song Name: "+tempSongName + "\n");
+            console.log("Preview Link-URL: "+tempURL+"\n");
+            console.log("Album Name: "+tempAlbumName + "\n");
+
+            var getSpotifylog= {
+                    searchCommand1:searchOption,
+                    searchCommand2:searchTitle,
+                    ArtistName: tempArtistName,
+                    SongName: tempSongName,
+                    PreviewLinkURL:tempURL,
+                    AlbumName:tempAlbumName
+                }
+
+            writeTolog(getSpotifylog)
+
+        });
+    
     };
-} else if (action == "do-what-it-says") {
-    command();
-} else {
-    console.log("Invalid command");
+
+    //OMDB API
+    function getMovie(){
+        var title = "";
+        // if user has search Title use it
+        if(searchTitle){
+            title = searchTitle;
+        }
+        // else use Mr.Nobody as default title.
+        else{
+            title = "Mr. Nobody";
+        }
+    
+            request("http://www.omdbapi.com/?&apikey=d6b01163"+"&t="+title,function(error, response, body) {
+            // If the request was successful...
+            if (!error && response.statusCode === 200) {
+            
+                var result = JSON.parse(body);
+                // console.log(result);
+                console.log("Title of Movie: "+result.Title +"\n");
+                
+                console.log("Year released: "+result.Year +"\n");
+                console.log("IMDB Rating: "+result.imdbRating+"\n");
+                // not all movies have rotten tomatoes rating... 
+                if(result.Ratings[1]!=null){
+                console.log("Rotten Tomatoes Rating: "+result.Ratings[1].Value+"\n");}                                      
+                console.log("Country: "+result.Country+"\n");
+                console.log("Language: "+result.Language+"\n");
+                console.log("Plot: "+result.Plot+"\n");
+                console.log("Actors: "+result.Actors+"\n");
+            }
+
+                var getMovielog= {
+                        searchCommand1:searchOption,
+                        searchCommand2:searchTitle,
+                        Title: result.Title,
+                        Year: result.Year,
+                        RatingIMDB:result.imdbRating,
+                        Country:result.Country,
+                        Plot:result.Plot,
+                        Actors:[result.Actors]
+                    }
+
+                writeTolog(getMovielog)
+
+      
+    });
 };
+
+    //file system(fs) to read argument/searchTitle from random.txt file 
+    function getDoWhatItSays(){
+
+            fs.readFile("random.txt","utf8",function(err,data){
+        
+                if(err){
+                    console.log(err)
+                }
+                data = data.split(",");
+
+                console.log(data);// for reference only
+        
+                switch(data[0]){
+                    case "my-tweets":
+                    getTweets();
+                    break;
+        
+                    case "spotify-this-song":
+                    searchTitle = data[1];
+                    getSpotify();
+                    break;
+                    
+                    case "movie-this":
+                    searchTitle = data[1];
+                    getMovie();
+                    break;
+                }
+            })  
+        };
+
+    //BONUS In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
+
+    function writeTolog(data) {
+
+        fs.appendFile("log.txt",JSON.stringify(data)+"\n\n", function(err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("your search Items and results will be saved under log.txt");
+        });
+    }
